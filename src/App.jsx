e8f78a53959e5667cd58289c4c5aa33c9b72949d
Bipin Singh <bipinsingh8052@gmail.com>
2025-01-02 18:16:25 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Login from "./Login"
+import SignUp_Form from "./SignUp_Form"
+
+
+function App() {
+  return (
+    <>
+      {/* <SignUp_Form/> */}
+      <Login/>
+    </>
+  )
+}
+
+export default App
